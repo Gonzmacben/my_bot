5de91340631889b2b709ff4d7b4dd9335c0ff968
Gonzmacben <gonzmacben02@gmail.com>
2025-06-02 21:56:29 -0600

    pid softener

--- firmware/motor_bridge/motor_bridge.ino
+++ firmware/motor_bridge/motor_bridge.ino
@@ -12,7 +12,7 @@ int16_t target_speed_ticks = 0;  // ticks per PID loop (speed)
 int pwm_output = 0;
 
 // PID Parameters
-float Kp = 10;
+float Kp = 12;
 float Ki = 0.05;
 float Kd = 0.8;
 float Ko = 1.0;
