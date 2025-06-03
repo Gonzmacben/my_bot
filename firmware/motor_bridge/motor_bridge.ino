bd473321679a4a5394251a879c4a2d1582a77dc2
Gonzmacben <gonzmacben02@gmail.com>
2025-06-02 19:51:25 -0600

    pid tweak 4

--- firmware/motor_bridge/motor_bridge.ino
+++ firmware/motor_bridge/motor_bridge.ino
@@ -12,7 +12,7 @@ int16_t target_speed_ticks = 0;  // ticks per PID loop (speed)
 int pwm_output = 0;
 
 // PID Parameters
-float Kp = 3.0;
+float Kp = 5.0;
 float Ki = 0.0;
 float Kd = 0.625;
 float Ko = 1.0;
